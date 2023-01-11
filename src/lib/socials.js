a48d87e74ce07793a7996c70576c851a801aeb1f
Moses Fawole <mosesfawole2001@gmail.com>
2023-01-11 20:11:24 +0100

    create data for social media links

---
+++ src/lib/socials.js
@@ -0,0 +1,19 @@
+import { FaGithub, FaEnvelope, FaTwitter } from "react-icons/fa";
+const socials = [
+  {
+    name: "Github",
+    href: "www.github.com/mosesfawole",
+    logo: <FaGithub />,
+  },
+  {
+    name: "Twitter",
+    href: "www.twitter.com/pharwoleh",
+    logo: <FaTwitter />,
+  },
+  {
+    name: "Email",
+    href: "mailto:phaworleh.dev@gmail.com",
+    logo: <FaEnvelope />,
+  },
+];
+export default socials;
