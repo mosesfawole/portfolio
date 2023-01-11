9c991a745d77cf41f2f4cb609c9dd5c99062c71c
Moses Fawole <mosesfawole2001@gmail.com>
2023-01-11 20:49:33 +0100

    projects page revamp

---
+++ src/lib/projects.js
@@ -0,0 +1,55 @@
+const projects = [
+  {
+    title: "Syncfusion Dashboard",
+    description: "Functional Dashboard created using Syncfusion ui comonents",
+    github: "https://github.com/mosesfawole/syncfusion-dashboard",
+    live: "https://syncfusion-dashboard-gamma.vercel.app/",
+    technology: "React js + Syncfusion components + Tailwind ",
+  },
+  {
+    title: "Ecommerce store",
+    description: "Ecommerce web app with stripe payment gateway",
+    github: "https://github.com/mosesfawole/ecommerce",
+    live: "https://ecommerce-mosesfawole.vercel.app/",
+    technology: "Next js + Sanity cms + stripe",
+  },
+  {
+    title: "Todo App",
+    description: "A simple todo app with user authentication  ",
+    github: "https://github.com/mosesfawole/checkmate",
+    live: "https://mf-todo-app.herokuapp.com/login",
+    technology:
+      "MongoDb + Express js + React js + Node js + Redux template + Toastify",
+  },
+  {
+    title: "Countries in the world",
+    description: "Frontend mentor challenge",
+    github: "https://github.com/mosesfawole/countries-api",
+    live: "https://countries-api-tau-blush.vercel.app",
+    technology: "React + Tailwind Css + framer-motion ",
+  },
+
+  {
+    title: "Recipe App",
+    description: "Food recipe app",
+    github: "https://github.com/mosesfawole/recipe-app",
+    live: "https://mf-recipe-app.netlify.app/",
+    technology: "React + Tailwind Css + framer-motion + styled-components",
+  },
+  {
+    title: " Snap",
+    description: "frontend mentor challenge, with dropdown and sidepanel",
+    github: "https://github.com/mosesfawole/snap",
+    live: "https://mf-snap.netlify.app",
+    technology: "React + Tailwind Css + HeadlessUi",
+  },
+  {
+    title: " Quiz App",
+    description: "A simple quiz app ",
+    github: "https://github.com/mosesfawole/quiz-app",
+    live: "https://mf-quiz-app.netlify.app/",
+    technology: "React-Typescript template + styled-components",
+  },
+];
+
+export default projects;
